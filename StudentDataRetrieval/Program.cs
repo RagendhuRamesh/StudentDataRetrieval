95a3b30885d74b57002964ce215c50c3356a4e5e
RagendhuRamesh <ragendhuram@gmail.com>
2023-12-16 13:37:52 +0530

    Add project files.

---
+++ StudentDataRetrieval/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "C:\\Users\\Ragendhu\\source\\repos\\StudentDataRetrieval\\StudentDataRetrieval\\students.txt";
+
+        try
+        {
+            // Read all lines from the file
+            string[] lines = File.ReadAllLines(filePath);
+
+            // Display each line (student data)
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found. Make sure the file exists and try again.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
